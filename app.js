2362e20a9e306066890fb8f537dd79de132aa13f
ghigbie <gehigbie@gmail.com>
2017-02-24 21:35:56 +0000

    added index.ejs, header.ejs and footer.ejs

--- app.js
+++ app.js
@@ -3,10 +3,28 @@ var express = require("express"),
     mongoose = require("mongoose"),
     bodyParser = require("body-parser");
     
-//mongoose.connect("mongodb://localhost/restful_blog_app");
+mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+// Mongoose/modle config
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {
+        type: Date, 
+        default: Date.now()
+    }
+});
+var Blog = mongoose.model("Blog", blogSchema);
+
+//RESTFUL ROUTES
+
+app.get("/blogs", function(req, res){
+   res.render("index"); 
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("SERVER IS RUNNING!!!");
